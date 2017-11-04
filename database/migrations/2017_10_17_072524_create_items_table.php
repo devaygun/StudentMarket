c9c31a0b67e5a8bfe994acd26e7d4e6938b6ab6d
Sam Wood <sw517@kent.ac.uk>
2017-11-04 17:24:39 +0000

    Added database columns for item table

--- database/migrations/2017_10_17_072524_create_items_table.php
+++ database/migrations/2017_10_17_072524_create_items_table.php
@@ -17,8 +17,13 @@ class CreateItemsTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->integer('requested_price');
+            $table->string('exchange_type');
             $table->string('item_name');
+            $table->string('description');
+            $table->integer('requested_price');
+            $table->string('requested_item');
+            $table->string('category');
+            $table->string('tags');
             $table->timestamps();
         });
     }
