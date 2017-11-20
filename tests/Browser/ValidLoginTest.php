fa909a299634bbc3bd4ba7f497ae47311149f64e
= <=>
2017-11-20 22:26:03 +0000

    Debug

--- tests/Browser/ValidLoginTest.php
+++ tests/Browser/ValidLoginTest.php
@@ -18,6 +18,11 @@ class ValidLoginTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->visit('/login')
                 ->assertSee('login')
+                ->type('email','rd339@kent.ac.uk')
+                ->type('password','richard123')
+                ->clickLink('Login')
+                ->waitUntil(10)
+                ->assertSee('Password')
             ->assertPathIs('/login');
         });
     }
