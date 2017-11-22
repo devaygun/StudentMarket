76daafe132474f4c9824df35c00b47c6bcaa69f6
Deniz <denizaygun@hotmail.co.uk>
2017-11-22 11:05:24 +0000

    Debug

---
+++ tests/Browser/TestItemSale.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class TestItemSales extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testItemSale()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->assertSee('login')
+                ->type('email','rd339@kent.ac.uk') //valid credentials
+                ->type('password','richard123')    //valid credentials
+                ->click('button[type="submit"]')
+                ->clickLink('Sell Item')
+                ->assertSee('price');
+        });
+    }
+}
