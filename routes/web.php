642a8a1c28382a71951d7093247927a5595a6b56
Deniz <denizaygun@hotmail.co.uk>
2017-12-11 21:43:45 +0000

    Began working on item search feature

--- routes/web.php
+++ routes/web.php
@@ -40,12 +40,12 @@ Route::post('/item/{id}/remove', 'ItemController@removeItem');
 Route::post('/images/remove', 'ImageController@delete');
 
 
+
+/* Search Routes */
+Route::get('/search/{string}', 'SearchController@index');
+
+
 /* User Routes */
 Route::get('/view/{id}', 'UserController@viewUser');
 Route::get('/profile', 'UserController@index');
 Route::post('/profile', 'UserController@update')->name('update_profile');
-
-
-Route::get('blade', function () {
-    return view('items.add_modal');
-});
