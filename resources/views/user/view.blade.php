372db107aba8b9f0ebce302fc78fb14a7f5f0c11
RichD <rd339@kent.ac.uk>
2017-12-12 06:53:54 +0000

    Added additional functionality to profile views

--- resources/views/user/view.blade.php
+++ resources/views/user/view.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-
+        <div class="row">
             <div class="col-sm-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">{{$user->first_name}} {{$user->last_name}}</div>
@@ -21,6 +21,7 @@
                                     <tr><th>Email Address: {{$user->email}}</th></tr>
                                     <tr><th>Rating:</th></tr>
                                     <tr><th>Member since: {{$user->created_at->format('jS \\ F Y')}}</th></tr>
+                                    <tr><th>Items: {{$user->items->pluck('name')}}</th></tr>
                                 </thead>
                             </table>
                         </div>
@@ -36,4 +37,5 @@
             </div>
         </div>
     </div>
+ </div>
 @endsection
