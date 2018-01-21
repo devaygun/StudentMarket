379ca5aa6cae40d83afd1e7e578167ed8cef6bd6
= <=>
2018-01-21 17:24:28 +0000

    Removed vue.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,6 +15,6 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-const app = new Vue({
-    el: '#app'
-});
+// const app = new Vue({
+//     el: '#app'
+// });
