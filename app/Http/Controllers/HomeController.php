08069c32c53ae37b6d6f301819b17b38a2f692d2
Deniz <denizaygun@hotmail.co.uk>
2018-01-22 18:01:25 +0000

    Fixed issues with the navigation bar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
     }
 
     /**
@@ -25,12 +25,5 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
-    }
-
-    public function samMethod()
-    {
-        $user = Auth::user();
-
-        return view('user_profile', ['user' => $user]);
     }
 }
