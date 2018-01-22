89922c531a0f99fcd612fba841d1a41928b138a6
RichD <rd339@kent.ac.uk>
2018-01-22 17:36:47 +0000

    Fixed profile view page, added functionality for no item listings

--- resources/views/user/view.blade.php
+++ resources/views/user/view.blade.php
@@ -2,28 +2,43 @@
 
 @section('content')
     <div class="container">
-
-            <div class="col-sm-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">{{$user->first_name}} {{$user->last_name}}</div>
-                    <div class="row">
-                        <div class="col-sm-4">
-                            @if ($user->profile_picture)
-                                <img src="{{asset('storage/' . $user->profile_picture)}}" class="img-responsive" alt="">
-                            @else
-                                <img src="{{asset('images/default_profile.jpg')}}" class="img-responsive" alt="">
-                            @endif
-                        </div>
-                        <div class="col-sm-8">
-                            <table class="table table-striped">
-                                <thead>
-                                    <th></th>
-                                    <tr><th>Email Address: {{$user->email}}</th></tr>
-                                    <tr><th>Rating:</th></tr>
-                                    <tr><th>Member since: {{$user->created_at->format('jS \\ F Y')}}</th></tr>
-                                </thead>
-                            </table>
-                        </div>
+        <div class="col-sm-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">{{$user->first_name}} {{$user->last_name}}</div>
+                <div class="row">
+                    <div class="col-sm-4">
+                        @if ($user->profile_picture)
+                            <img src="{{asset('storage/' . $user->profile_picture)}}" class="img-responsive" alt="">
+                        @else
+                            <img src="{{asset('images/default_profile.jpg')}}" class="img-responsive" alt="">
+                        @endif
+                    </div>
+                    <div class="col-sm-8">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>Email Address: {{$user->email}}</th>
+                            </tr>
+                            <tr>
+                                <th>Rating:</th>
+                            </tr>
+                            <tr>
+                                <th>Member since: {{$user->created_at->format('jS \\ F Y')}}</th>
+                            </tr>
+                            <tr>
+                                <th>Items:
+                                    @if ($user->items->isEmpty())
+                                        No items listed
+                                    @else
+                                        @foreach($user->items->pluck('name') as $item)
+                                            {{$item}},
+                                        @endforeach
+                                    @endif
+                                </th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </div>
 
                     <div class="panel-body">
                         @if (session('status'))
