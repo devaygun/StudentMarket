6442d8fb911c34b89c18e1efc77cc61e9dd08ece
Deniz <denizaygun@hotmail.co.uk>
2018-02-02 17:45:52 +0000

    Added item factories (for testing purposes)

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -4,6 +4,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Item::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => 1,
+        'category_id' => 1,
+        'name' => $faker->sentence(2),
+        'description' => $faker->realText(50),
+        'type' => 'sell',
+        'price' => $faker->numberBetween(1, 100),
+        'trade' => null,
     ];
 });
