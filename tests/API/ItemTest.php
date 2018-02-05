65ae788976b05cf717ede753279019cc1fe5a298
Deniz <denizaygun@hotmail.co.uk>
2018-02-05 14:42:54 +0000

    Added additional API based tests

---
+++ tests/API/ItemTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ItemTest extends TestCase
+{
+    /**
+     * Basic test to check if retrieving all items works
+     *
+     * @return void
+     */
+    public function testItemOverview()
+    {
+        $api_token = User::find(1)->api_token;
+        $response = $this->get("/api/items?api_token=$api_token");
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Basic test to check if retrieving an individual items works
+     *
+     * @return void
+     */
+    public function testItemRead()
+    {
+        $api_token = User::find(1)->api_token;
+        $response = $this->get("/api/items/appliances/1?api_token=$api_token");
+
+        $response->assertStatus(200);
+    }
+}
