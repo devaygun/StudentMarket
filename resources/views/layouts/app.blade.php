5ad31a289f294dba38aaf8ca98b942a7d83382d4
Deniz <denizaygun@hotmail.co.uk>
2018-03-01 19:25:54 +0000

    Added favicon support

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="https://student-market.co.uk/favicon.ico" />
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('page_title') {{ config('app.name', 'Student Market') }}</title>
