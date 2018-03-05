09e913c9e695f733fece26fb8daba3bd64c6bb99
Deniz <denizaygun@hotmail.co.uk>
2018-03-05 12:54:20 +0000

    Fixed an API issue

--- app/Http/Requests/RegistrationRequest.php
+++ app/Http/Requests/RegistrationRequest.php
@@ -13,7 +13,7 @@ class RegistrationRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -28,7 +28,6 @@ class RegistrationRequest extends FormRequest
             'last_name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'date_of_birth' => 'required|date',
-            'distance_unit' => 'required',
             'profile_picture' => 'image|dimensions:min_width=100,min_height=100,max_width=2000,max_height=2000',
             'password' => 'required|string|min:6|confirmed', // This also means that in the API request a "password_confirmation" input must be supplied
         ];
