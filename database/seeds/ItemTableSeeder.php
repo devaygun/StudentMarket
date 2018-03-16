03ac88dbe21a9226a889423cae58f1ba65ac9889
Deniz <denizaygun@hotmail.co.uk>
2018-03-16 11:29:23 +0000

    Minor changes

--- database/seeds/ItemTableSeeder.php
+++ database/seeds/ItemTableSeeder.php
@@ -13,137 +13,137 @@ class ItemTableSeeder extends Seeder
      */
     public function run()
     {
-
-        DB::table('items')->insert([
-            'user_id' => 2,
-            'category_id' => 2,
-            'name' => 'Sony Speakers',
-            'description' => "5:1 Sony Speakers for PC, used but good condition",
-            'type' => 'sell',
-            'price' => 30,
-            'trade' => null,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('items')->insert([
-            'user_id' => 2,
-            'category_id' => 2,
-            'name' => 'Running Trainers',
-            'description' => "Size 10 outdoor running trainers",
-            'type' => 'swap',
-            'price' => null,
-            'trade' => "Winter Jacket",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('items')->insert([
-            'user_id' => 2,
-            'category_id' => 2,
-            'name' => 'Guitar',
-            'description' => "Electric guitar, black and white, good condition",
-            'type' => 'swap',
-            'price' => null,
-            'trade' => "Ukulele",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('items')->insert([
-            'user_id' => 2,
-            'category_id' => 2,
-            'name' => 'Philosophy Textbook',
-            'description' => "Modularity of the mind textbook",
-            'type' => 'part-exchange',
-            'price' => 10,
-            'trade' => "USB 3.0 Flash Drive",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('items')->insert([
-            'user_id' => 2,
-            'category_id' => 2,
-            'name' => 'Headphones',
-            'description' => "Red headphones, brand new in packaging",
-            'type' => 'swap',
-            'price' => null,
-            'trade' => "Acoustic Guitar",
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('items')->insert([
-            'user_id' => 2,
-            'category_id' => 2,
-            'name' => 'Laptop',
-            'description' => "15\" laptop, i5 Processor, 4GB RAM",
-            'type' => 'sell',
-            'price' => 200,
-            'trade' => null,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('items')->insert([
-            'user_id' => 1,
-            'category_id' => 1,
-            'name' => 'Panasonic Microwave',
-            'description' => 'A large modern 1250W microwave.',
-            'type' => 'sell',
-            'price' => 45,
-            'trade' => null,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('items')->insert([
-            'user_id' => 1,
-            'category_id' => 1,
-            'name' => 'Russel Hobbs Kettle',
-            'description' => 'Great valued kettle.',
-            'type' => 'sell',
-            'price' => 4,
-            'trade' => null,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('items')->insert([
-            'user_id' => 1,
-            'category_id' => 3,
-            'name' => 'Getting Things Done - David Allen',
-            'description' => "Discover David Allen's powerful methods for stress-free performance at work and in life.",
-            'type' => 'sell',
-            'price' => 7,
-            'trade' => null,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('items')->insert([
-            'user_id' => 3,
-            'category_id' => 3,
-            'name' => 'Book',
-            'description' => "An old book",
-            'type' => 'sell',
-            'price' => 5,
-            'trade' => null,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
-
-        DB::table('items')->insert([
-            'user_id' => 2,
-            'category_id' => 3,
-            'name' => 'Apollo Bike',
-            'description' => "Fairly new apollo bike. Ridden a few times, looks brand new.",
-            'type' => 'part-exchange',
-            'price' => 40,
-            'trade' => 'Skateboard',
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now()
-        ]);
+//
+//        DB::table('items')->insert([
+//            'user_id' => 2,
+//            'category_id' => 2,
+//            'name' => 'Sony Speakers',
+//            'description' => "5:1 Sony Speakers for PC, used but good condition",
+//            'type' => 'sell',
+//            'price' => 30,
+//            'trade' => null,
+//            'created_at' => Carbon::now(),
+//            'updated_at' => Carbon::now()
+//        ]);
+//
+//        DB::table('items')->insert([
+//            'user_id' => 2,
+//            'category_id' => 2,
+//            'name' => 'Running Trainers',
+//            'description' => "Size 10 outdoor running trainers",
+//            'type' => 'swap',
+//            'price' => null,
+//            'trade' => "Winter Jacket",
+//            'created_at' => Carbon::now(),
+//            'updated_at' => Carbon::now()
+//        ]);
+//
+//        DB::table('items')->insert([
+//            'user_id' => 2,
+//            'category_id' => 2,
+//            'name' => 'Guitar',
+//            'description' => "Electric guitar, black and white, good condition",
+//            'type' => 'swap',
+//            'price' => null,
+//            'trade' => "Ukulele",
+//            'created_at' => Carbon::now(),
+//            'updated_at' => Carbon::now()
+//        ]);
+//
+//        DB::table('items')->insert([
+//            'user_id' => 2,
+//            'category_id' => 2,
+//            'name' => 'Philosophy Textbook',
+//            'description' => "Modularity of the mind textbook",
+//            'type' => 'part-exchange',
+//            'price' => 10,
+//            'trade' => "USB 3.0 Flash Drive",
+//            'created_at' => Carbon::now(),
+//            'updated_at' => Carbon::now()
+//        ]);
+//
+//        DB::table('items')->insert([
+//            'user_id' => 2,
+//            'category_id' => 2,
+//            'name' => 'Headphones',
+//            'description' => "Red headphones, brand new in packaging",
+//            'type' => 'swap',
+//            'price' => null,
+//            'trade' => "Acoustic Guitar",
+//            'created_at' => Carbon::now(),
+//            'updated_at' => Carbon::now()
+//        ]);
+//
+//        DB::table('items')->insert([
+//            'user_id' => 2,
+//            'category_id' => 2,
+//            'name' => 'Laptop',
+//            'description' => "15\" laptop, i5 Processor, 4GB RAM",
+//            'type' => 'sell',
+//            'price' => 200,
+//            'trade' => null,
+//            'created_at' => Carbon::now(),
+//            'updated_at' => Carbon::now()
+//        ]);
+//
+//        DB::table('items')->insert([
+//            'user_id' => 1,
+//            'category_id' => 1,
+//            'name' => 'Panasonic Microwave',
+//            'description' => 'A large modern 1250W microwave.',
+//            'type' => 'sell',
+//            'price' => 45,
+//            'trade' => null,
+//            'created_at' => Carbon::now(),
+//            'updated_at' => Carbon::now()
+//        ]);
+//
+//        DB::table('items')->insert([
+//            'user_id' => 1,
+//            'category_id' => 1,
+//            'name' => 'Russel Hobbs Kettle',
+//            'description' => 'Great valued kettle.',
+//            'type' => 'sell',
+//            'price' => 4,
+//            'trade' => null,
+//            'created_at' => Carbon::now(),
+//            'updated_at' => Carbon::now()
+//        ]);
+//
+//        DB::table('items')->insert([
+//            'user_id' => 1,
+//            'category_id' => 3,
+//            'name' => 'Getting Things Done - David Allen',
+//            'description' => "Discover David Allen's powerful methods for stress-free performance at work and in life.",
+//            'type' => 'sell',
+//            'price' => 7,
+//            'trade' => null,
+//            'created_at' => Carbon::now(),
+//            'updated_at' => Carbon::now()
+//        ]);
+//
+//        DB::table('items')->insert([
+//            'user_id' => 3,
+//            'category_id' => 3,
+//            'name' => 'Book',
+//            'description' => "An old book",
+//            'type' => 'sell',
+//            'price' => 5,
+//            'trade' => null,
+//            'created_at' => Carbon::now(),
+//            'updated_at' => Carbon::now()
+//        ]);
+//
+//        DB::table('items')->insert([
+//            'user_id' => 2,
+//            'category_id' => 3,
+//            'name' => 'Apollo Bike',
+//            'description' => "Fairly new apollo bike. Ridden a few times, looks brand new.",
+//            'type' => 'part-exchange',
+//            'price' => 40,
+//            'trade' => 'Skateboard',
+//            'created_at' => Carbon::now(),
+//            'updated_at' => Carbon::now()
+//        ]);
     }
 }
