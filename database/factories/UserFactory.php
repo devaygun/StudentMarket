f585b26d6f66141d2defa49279a94c967cab6c57
Deniz <denizaygun@hotmail.co.uk>
2018-03-23 21:09:37 +0000

    Removed unused code

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,14 +16,11 @@ use Faker\Generator as Faker;
 $factory->define(App\User::class, function (Faker $faker) {
     static $password;
 
-    $profile_pictures = ['demo_1.jpg','demo_2.jpg','demo_3.jpg','demo_4.jpg','demo_5.jpg','demo_6.jpg','demo_7.jpg','demo_8.jpg','demo_9.jpg','demo_10.jpg'];
-
     return [
         'first_name' => $faker->firstName,
         'last_name' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'date_of_birth' => $faker->dateTimeThisCentury,
-//        'profile_picture' => "profiles/" . $profile_pictures[rand(0,8)], //array_rand($profile_pictures),
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
         'api_token' => str_random(60),
